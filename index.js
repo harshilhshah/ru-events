c6b1c5ca2cb069cfe8f61491deccde8e691177e9
Harshil Shah <harshilhshah1995@gmail.com>
2016-10-23 15:59:07 -0400

    quite done

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var pg = require('pg');
+var dotenv = require('dotenv');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -10,11 +12,30 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+	dotenv.load();
+	var events = [];
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+
+		var handleError = function(err) {
+			if(!err) return false;
+			done(client);
+			next(err);
+			return true;
+		};
+
+		client.query('SELECT * FROM events', function(err, result) {
+			events = result.rows;
+			done();
+			if(err) return console.error(err);
+			response.render('pages/index',{t:events,events_view:events});
+		});
+	});
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
+
+
 
 
